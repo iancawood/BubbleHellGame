04d2aec781f48a00cb8bc6df6efd645af2ad1769
Ian Wood <ian.wood108@gmail.com>
2017-03-17 00:00:35 -0400

    Basic bubble spawner and bubble

--- Assets/Scripts/Game/Bubble.cs
+++ Assets/Scripts/Game/Bubble.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bubble : MonoBehaviour {
+    float SPEED = 15f;
+    float DEATH_ZONE = -10f; // Where the bubbles despawn, should be negative
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update () {
+        transform.position += Vector3.back * Time.deltaTime * SPEED;
+
+        if (transform.position.z < DEATH_ZONE) {
+            Destroy(this.gameObject);
+        }
+    }
 }
--- Assets/Scripts/Game/BubbleSpawner.cs
+++ Assets/Scripts/Game/BubbleSpawner.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class BubbleSpawner : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject bubble;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    float zPos;
+
+    float spawnRate = 0.5f; // delay between bubbles spawns
+    float nextSpawn = 0;
+
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+        Collider collider = this.GetComponent<Collider>();
+        xMin = collider.bounds.min.x;
+        xMax = collider.bounds.max.x;
+        yMin = collider.bounds.min.y;
+        yMax = collider.bounds.max.y;
+        zPos = collider.bounds.center.z;
+    }
+
 	void Update () {
-		
-	}
+        nextSpawn -= Time.deltaTime;
+
+        if (nextSpawn <= 0) {
+            nextSpawn = spawnRate;
+            spawnBubble();
+        }
+    }
+
+    void spawnBubble() {
+        Vector3 position = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), zPos);
+
+        Instantiate(bubble, position, Quaternion.identity);
+    }
 }
